d0bf690d78cb4b31b7bea0445aefba753f2e0a8d
selgadis <osmargastonescobar@gmail.com>
2024-06-15 19:59:21 -0300

    funcion  verificar nombre apellido  email

--- js/registro.js
+++ js/registro.js
@@ -1,6 +1,10 @@
 let nombreInput =document.querySelector("#nombre");//Es el input donde ingreso el nombre
 let apellidoInput =document.querySelector("#apellido");
 let correoInput=document.querySelector("#email")
+let nombreError=document.querySelector("#nombreError");
+let apellidoError=document.querySelector("#apellidoError");
+let emailError=document.querySelector("#emailError");
+
 const usuario = {
     nombre: "",
     apellido: "",
@@ -9,31 +13,60 @@ const usuario = {
     contraseña: ""
 };
 
-function verificar(e) {
+function verificarNombre(e) {
     const regex = /^[a-zA-Z]+$/;
     const input = e.target; // El elemento que disparó el evento
     if (!regex.test(input.value)) {
-        // Eliminar caracteres no permitidos
-        input.value = input.value.replace(/[^a-zA-Z]/g, '');
+
+        nombreError.classList.remove("errorOculto");
+        nombreError.classList.add("errorOn");
+        usuario.nombre=null;
+        console.log(usuario.nombre);
     }else{
-        console.log(e.target.value);
+        nombreError.classList.remove("errorOn");
+        nombreError.classList.add("errorOculto");
+        usuario.nombre=input.value;
+        console.log(usuario.nombre);
     }
 }
+function verificarApellido(e) {
+    const regex = /^[a-zA-Z]+$/;
+    const input = e.target; // El elemento que disparó el evento
+    if (!regex.test(input.value)) {
+
+        apellidoError.classList.remove("errorOculto");
+        apellidoError.classList.add("errorOn");
+        usuario.apellido=null;
+        console.log(usuario.apellido);
+    }else{
+        apellidoError.classList.remove("errorOn");
+        apellidoError.classList.add("errorOculto");
+        usuario.apellido=input.value;
+        console.log(usuario.apellido);
+    }
+}
+
 function verificarCorreo(e){
     
     const regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     let input =e.target;
     if(!regexCorreo.test(input.value)){
-        console.log("correo invalido")
+        emailError.classList.remove("errorOculto");
+        emailError.classList.add("errorOn");
+        usuario.email=null;
+        console.log(usuario.email)
     }
     else{
-        console.log("correo correcto")
+       emailError.classList.remove("errorOn");
+       emailError.classList.add("errorOculto");
         usuario.email=input.value;
+        console.log(usuario.email);
+
     }
 }
-nombreInput.addEventListener("input",verificar);
-apellidoInput.addEventListener("input",verificar);
-correoInput.addEventListener("input",verificarCorreo);
+nombreInput.addEventListener("input",verificarNombre);
+apellidoInput.addEventListener("input",verificarApellido);
+ correoInput.addEventListener("input",verificarCorreo);
 
 
 
