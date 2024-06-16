415ca1da8fc3de8d0ed1fc5835d443e5cfef2260
selgadis <osmargastonescobar@gmail.com>
2024-06-16 17:42:53 -0300

    cambios en registrarse

--- js/registro.js
+++ js/registro.js
@@ -1,16 +1,26 @@
 let nombreInput =document.querySelector("#nombre");//Es el input donde ingreso el nombre
 let apellidoInput =document.querySelector("#apellido");
-let correoInput=document.querySelector("#email")
+let correoInput=document.querySelector("#email");
+let usuarioInput=document.querySelector("#nombreUsuario");
 let nombreError=document.querySelector("#nombreError");
 let apellidoError=document.querySelector("#apellidoError");
 let emailError=document.querySelector("#emailError");
+let usuarioError=document.querySelector("#usuarioError");
+let contraseniaInput=document.querySelector("#contrasenia");
+let contraseniaInput2=document.querySelector("#contrasenia2");
+let contraseniaError=document.querySelector("#contrasenia2Error")
+let tarjetaInput=document.querySelector("#numero_tarjeta");
+let tarjetaError=document.querySelector("#tarjetaError");
+let cifraError=document.querySelector("#cifraError");
+let contraseniaRespaldo=null;
 
 const usuario = {
     nombre: "",
     apellido: "",
     email: "",
     nombreDeUsuario: "",
-    contraseña: ""
+    contraseña: "",
+    tarjeta:""
 };
 
 function verificarNombre(e) {
@@ -57,16 +67,76 @@ function verificarCorreo(e){
         console.log(usuario.email)
     }
     else{
-       emailError.classList.remove("errorOn");
-       emailError.classList.add("errorOculto");
+        emailError.classList.remove("errorOn");
+        emailError.classList.add("errorOculto");
         usuario.email=input.value;
         console.log(usuario.email);
 
     }
 }
+function verificarUsuario(e){
+    const regexUsuario= /^[a-zA-Z0-9]+$/;
+    let input =e.target;
+    if(!regexUsuario.test(input.value)){
+        usuarioError.classList.remove("errorOculto");
+        usuarioError.classList.add("errorOn");
+        usuario.nombreDeUsuario=null;
+        console.log(usuario.nombreDeUsuario);
+    }
+    else{
+        usuarioError.classList.remove("errorOn");
+        usuarioError.classList.add("errorOculto");
+        usuario.nombreDeUsuario=input.value;
+        console.log(usuario.nombreDeUsuario);
+    }
+
+}
+function verificarContasenia(e){
+    const input =e.target;
+    contraseniaRespaldo=input.value;
+
+}
+function verificarContasenia2(e){
+    const input =e.target;
+    if(contraseniaRespaldo===input.value){
+        usuario.contraseña=input.target;
+        contraseniaError.classList.remove("#errorOn");
+        contraseniaError.classList.add("errorOculto");
+        usuario.contraseña=input.value;
+        console.log(usuario.contraseña);
+    }
+    else{
+        contraseniaError.classList.remove("errorOculto");
+        contraseniaError.classList.add("#errorOn");
+        usuario.contraseña=null;
+        console.log(usuario.contraseña);
+    }
+}
+
+function verificarTarjeta(e){
+    const regexTarjeta= /^\d{16,19}$/;
+    const input=e.target;
+    if(!regexTarjeta.test(input.value)){
+        tarjetaError.classList.remove("errorOculto");
+        tarjetaError.classList.add("errorOn");
+        usuario.tarjeta=null;
+        console.log(usuario.tarjeta);
+    }else{
+        tarjetaError.classList.remove("errorOn");
+        tarjetaError.classList.add("errorOculto");
+        usuario.tarjeta=input.value;
+        console.log(usuario.tarjeta);
+    }
+
+}
+
 nombreInput.addEventListener("input",verificarNombre);
 apellidoInput.addEventListener("input",verificarApellido);
- correoInput.addEventListener("input",verificarCorreo);
+correoInput.addEventListener("input",verificarCorreo);
+usuarioInput.addEventListener("input",verificarUsuario);
+contraseniaInput.addEventListener("input",verificarContasenia);
+contraseniaInput2.addEventListener("input",verificarContasenia2);
+tarjetaInput.addEventListener("input",verificarTarjeta);
 
 
 
